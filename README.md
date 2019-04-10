4e4335059bc13afd25f6f98436bc205941f7e22f
Martin Hanusch <martin.hanusch@let.ethz.ch>
2019-04-10 11:33:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# xmlparser
+
+## What it is:
+A simple script that extracts all images of Moodle Course Backup files.
+The backup files must be in xml-format.
+
+
+## Installation:
+1. Install [Node.js](https://nodejs.org/en/)
+1. Switch into the xmlparser folder using the terminal.
+1. Run `npm install` to setup the script's dependencies.
+1. Make sure the executing user has sufficient permissions to access the _output folder in write-mode.
+
+
+## How the script works:
+1. Put all xml files you want to process into the _input folder.
+1. Enter a terminal and run `node script.js` or `npm start`.
+
+The script checks for all xml files in the _input folder.
+Each xml files images will be stored in the _output folder.
+
+
+## Possible errors that may occur:
+> [ERR] - ... is not a xml file.
+
+* One of the files in the _input folder is not a file of type xml.
+* All files in the input folder need to be in xml format.
+
+> [ERR] ... - Error: Non-whitespace before first tag.
+
+* The script opened the xml file but it does not contain xml content.
+This may be caused when the original backup has not been saved as xml or when somebody messed with the file's contents.
+
+> [ERR] TypeError: Cannot read property '...' of undefined
+
+* This error occurs if the xml's content is xml but it's filestructure is not in moodle format.
+* Otherwise there may be changes to the moodle backup filestructure that need to be applied to the script in order to work correctly again. This needs some fixes to the script.
---
+++ script.js
@@ -0,0 +1,230 @@
+const fs = require('fs');
+const parser = require("xml2js").Parser({ attrkey: "key" });
+const $ = require('cheerio').load('<container></container>');
+
+
+// Sets up some basic information and variables
+var input = [];
+var output = [];
+var date = new Date(Date.now());
+var foldername = ("0" + date.getDate()).slice(-2) + "." +
+    ("0" + (date.getMonth() + 1)).slice(-2) + "." + 
+    date.getFullYear() + "-" +
+    ("0" + date.getHours()).slice(-2) + "." +
+    ("0" + date.getMinutes()).slice(-2) + " - " +
+    Math.random().toString(36).substr(2, 9).toUpperCase();
+
+console.log("Script started...");
+
+// Step 1: Creates a folder for this Script execution.
+// All processed xml contents will be stored within
+// Step 2: Reads the contents of the _input-folder.
+// Checks for the right extension (xml). Each file
+// represents a backup with and will be pushed into the
+// input array.
+// Step 3: Calls a function for each index of the input 
+// array, that further processes the input files.
+
+fs.readdir('_input/', (error, files)  => {
+    if (error) {
+        return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + error);
+    }
+    try {
+        // Step 1
+        fs.mkdir("_output/" + foldername, (error) => {
+            if (error) {
+                return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + error);
+            }
+        });
+
+        console.log("\n======================================");
+        console.log("Reading '_input'-folder (" + files.length + " files)...");
+        console.log("======================================");
+        // Step 2
+        files.forEach((file) => {
+            if (getExtension(file) == "xml") {
+                console.log("[\x1b[32m\x1b[1mOK \x1b[37m\x1b[0m] - '" + file + "' will be processed.");
+                input.push(file);
+            } else {
+                console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] - '" + file + "' is not a xml file.");
+            }
+        });
+
+        console.log("\n======================================");
+        console.log("Processing xml files...");
+        console.log("======================================");
+
+        // Step 3
+        for (var i = 0; i < input.length; i++) {
+            processXMLFile(input[i]);
+        }
+    } catch (error) {
+        return console.log("[\x1b[31m\x1b[0mERR\x1b[37m\x1b[0m] " + error);
+    }
+});
+
+// Step 1: Reads the contents of an xml file into a string.
+// Step 2: Parses the string into a json object.
+// Step 3: Iterates through the backup-json object.
+// Step 3.1: For each question: 
+// Creates an empty object in the output array.
+// Step 3.2: For each question: 
+// Pushes the questionname to the currently processed object.
+// Step 3.3. For each question:
+// Pushes the images to the currently processed object.
+// Step 4: Calls a function that stores all images to the filedisk
+
+processXMLFile = (xmlFile) => {
+    try {
+        // Step 1
+        fs.readFile('_input/' + xmlFile, 'utf8', (error, xmlcontent) => {
+            if (error) {
+                return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + xmlFile + " - " + error);
+            }
+            // Step 2
+            parser.parseString(xmlcontent, (error, result) => {
+                if (error) {
+                    return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + xmlFile + " - " + error);
+                }
+                for (var index in result.quiz.question) {
+                    // Step 3.1
+                    createQuestionIndex(index);
+                    // Step 3.2
+                    pushQuestiontext(result.quiz.question[index], index);
+                    // Step 3.3
+                    if(!output[index].error) {
+                        pushImages(result.quiz.question[index], index);
+                    }
+                }
+                // Step 4
+                saveImages(xmlFile);
+                console.log("[\x1b[32m\x1b[1mOK \x1b[37m\x1b[0m] - Extracted images of " + output.length + " questions into\n'_output/" + foldername + "/" + xmlFile + "'");
+            });
+        });
+    } catch (error) {
+        return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + error);  
+    }
+}
+
+createQuestionIndex = (index) => {
+    output[index] = {name: null, content: [], error: false};
+}
+
+pushQuestiontext = (question, index) => {
+    try {
+        if (question.name) {
+            output[index].name = question.name[0].text[0];
+        }
+    } catch (error) {
+        output[index].error = true;
+        return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + error);
+    }
+}
+
+
+pushImages = (question, index) => {
+    // (1) questiontext
+    // (1) questionfeedback
+    // (n) answer
+    // (n) answer -> (m) answerfeedback
+    try {
+        if (question.questiontext) {
+            // question.questiontext[0].text[0]
+            extractBase64(question.questiontext[0], index);
+         }
+         if (question.generalfeedback) {
+             // question.generalfeedback[0].text[0]
+             extractBase64(question.generalfeedback[0], index);
+         }
+         if (question.answer) {
+             for (var i in question.answer) {
+                 // question.answer[i].text[0]
+                 extractBase64(question.answer[i], index);
+     
+                 for(var j in question.answer[i].feedback) {
+                     // question.answer[i].feedback[j].text[0]
+                     extractBase64(question.answer[i].feedback[j], index);
+                 }
+             }
+         }
+    } catch (error) {
+        output[index].error = true;
+        return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + error);
+    }
+}
+
+// Step 1: Looks for base64 strings in questiontext -> text tag
+// Step 2: Looks for base64 strings in generalfeedback -> text tag
+// Step 3: Looks for base64 strings in the file tag
+// Note: All found strings ar pushed to the currently processed 
+// question object in the output array
+
+extractBase64 = (string, index) => {
+    try {
+        // Step 1
+        // Step 2
+        $("container").html(string.text[0]);
+
+
+        $("img").each(function () { 
+            if($(this).attr("src").indexOf("data:image/png;base64") !== -1) {
+                output[index].content.push($(this).attr("src").replace("data:image/png;base64,", ""));
+            } 
+            else if ($(this).attr("src").indexOf("data:image/jpg;base64") !== -1) {
+                output[index].content.push($(this).attr("src").replace("data:image/jpg;base64,", ""));
+            }
+            else if ($(this).attr("src").indexOf("data:image/gif;base64") !== -1) {
+                output[index].content.push($(this).attr("src").replace("data:image/gif;base64,", ""));
+            }
+        }); 
+        // Step 3
+        if (string.file) {
+            for(var i in string.file) {
+                output[index].content.push(string.file[i]["_"]);
+            }
+        }
+    } catch (error) {
+        return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + error);  
+    }
+}
+
+// Step 1: Creates a folder for the currently processed xml file.
+// Step 2.1: Loops over the output array (each index = question)
+// Step 2.2: Loops over output[index].content (each index = image)
+// Step 2.3: Saves each image to the filedisk
+
+saveImages = (xmlFile) => {
+    try {
+        // Step 1
+        fs.mkdir("_output/" + foldername + "/" + xmlFile, (error) => {
+            if (error) {
+                return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + xmlFile + " - " + error);
+            }
+        });
+        // Step 2.1
+        for (var i in output) {
+            if(!output[i].error && output[i].name != null && output[i].content.length > 0) {
+                // Step 2.2
+                var name = output[i].name.replace(/\//g, "-");
+                name = name.replace(/[^a-zA-Z0-9() ]/g, "");
+                name = name.replace(/\s/g,"_");
+
+                for (var j in output[i].content) {
+                    // Step 2.3
+                    fs.writeFile("_output/" + foldername + "/" + xmlFile + "/" + name + "_" + j + ".png", 
+                    output[i].content[j], 'base64', (error) => {
+                        if (error) {
+                            return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + xmlFile + " - " + error);
+                        }
+                    });
+                }
+            }
+        }
+    } catch (error) {
+        return console.log("[\x1b[31m\x1b[1mERR\x1b[37m\x1b[0m] " + error);     
+    }
+}
+
+getExtension = (filename) => {
+    return filename.split('.').pop();
+}
